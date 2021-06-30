4f3cfce7b4721262a12d65e6fe4f2b89ccd1c99d
jerife <jerife@naver.com>
2021-06-30 16:49:12 +0900

    feat. 서버 업로드 완료

--- Study_Web/ShoppingMall/app.py
+++ Study_Web/ShoppingMall/app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017)
+
+
 db = client.project
 
 ## HTML을 주는 부분
